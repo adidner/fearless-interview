91f8858de93a4656f55cd672d247c03e52d5cedd
Aaron Didner <adidner@gmail.com>
2021-07-19 20:48:25 -0400

    page load doesn't increment count now

--- src/apiCalls/CountApi.tsx
+++ src/apiCalls/CountApi.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const COUNT_API_KEY = "1ccb732e-b55a-4404-ad3f-0f99c02fe44e";
-const COUNT_API_BASE_URL = "https://api.countapi.xyz/hit/namespace/";
+const NAME_SPACE = "namespace/";
+const COUNT_API_BASE_URL = "https://api.countapi.xyz/";
 
 //get request to increment our count and get the newly incremented value via the CountApi
-export function incrementCountReturnValue(successCallback: (newValue: number) => void  , errorCallback: (errorMessage: string) => void ){
+export function incrementCountReturnValue(successCallback: (newValue: number) => void, errorCallback: (errorMessage: string) => void){
     axios
-    .get(COUNT_API_BASE_URL + COUNT_API_KEY)
+    .get(COUNT_API_BASE_URL + 'hit/' + NAME_SPACE + COUNT_API_KEY)
     .then((response) => {
         successCallback(response?.data?.value);
     })
@@ -15,3 +16,13 @@ export function incrementCountReturnValue(successCallback: (newValue: number) =>
     });
 }
 
+export function countReturnValue(successCallback: (newValue: number) => void, errorCallback: (errorMessage: string) => void){
+    axios
+    .get(COUNT_API_BASE_URL + 'get/' + NAME_SPACE + COUNT_API_KEY)
+    .then((response) => {
+        successCallback(response?.data?.value);
+    })
+    .catch(error => {
+        errorCallback(error.toString());
+    });
+}
