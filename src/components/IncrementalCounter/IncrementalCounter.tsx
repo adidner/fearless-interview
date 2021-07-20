cc6c651518dc3dd933c3f4a2cad22e3a0bf056db
Aaron Didner <adidner@gmail.com>
2021-07-19 21:30:36 -0400

    Spinner and loading text

--- src/components/IncrementalCounter/IncrementalCounter.tsx
+++ src/components/IncrementalCounter/IncrementalCounter.tsx
@@ -1,17 +1,23 @@
 import { incrementCountReturnValue, countReturnValue } from '../../apiCalls/CountApi';
 import { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export function IncrementalCounter() {
   
   const [countValue, setCountValue] = useState<number | undefined>(undefined);
   const [countError, setCountError] = useState<string>("");
+  const [loadingCount, setLoadingCount] = useState<boolean>(false);
+  
 
   const onSuccess = (newValue: number) => {
     setCountValue(newValue);
+    setLoadingCount(false);
   }
 
   const onError = (errorMessage: string) => {
     setCountError(errorMessage);
+    setLoadingCount(false);
   }
 
 
@@ -21,15 +27,33 @@ export function IncrementalCounter() {
 
 
   const handleIncrement = () => {
-    incrementCountReturnValue(setCountValue,setCountError);
+    setLoadingCount(true);
+    incrementCountReturnValue(onSuccess,onError);
   }
+
+  const showLoadingErrorOrCount = () => {
+    let message: string = "";
+    if(loadingCount){
+      message = "Content is loading this should only take a sec!";
+    }
+    else if(countError !== ""){
+      message = countError;
+    }
+    else{
+      message = countValue === undefined ? "" : countValue.toString();
+    }
+    return <div>{message}</div>
+  }
+
 
   return (
     <>
-        <div> {countError === "" ? countValue : countError} </div>
-        <button onClick={handleIncrement}>Increment Count</button>        
+        {loadingCount ? <CircularProgress/> : null}
+        {showLoadingErrorOrCount()}
+        <Button onClick={handleIncrement} variant={'contained'}>Increment Count</Button>        
     </>
   );
 }
+
 
 export default IncrementalCounter;
